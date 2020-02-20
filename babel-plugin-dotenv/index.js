f9b73902ff6e6d145e5754e510ff9819df767c74
Boris Burgos <bburgos@codeko.com>
2020-02-20 12:50:00 +0100

    feat: not defined values not throw exceptions anymore, default import allowed and .env*.local files supported

--- babel-plugin-dotenv/index.js
+++ babel-plugin-dotenv/index.js
@@ -4,45 +4,49 @@ var sysPath = require('path');
 var process = require('process');
 
 module.exports = function (data) {
-    var t = data.types;
+  var t = data.types;
 
-    return {
-        visitor: {
-            ImportDeclaration: function(path, state) {
-                var options = state.opts;
+  return {
+    visitor: {
+      ImportDeclaration: function(path, state) {
+        var options = state.opts;
 
-                if (options.replacedModuleName === undefined)
-                  return;
+        if (options.replacedModuleName === undefined)
+          return;
 
-                var configDir = options.configDir ? options.configDir : './';
-                var configFile = options.filename ? options.filename : '.env';
+        var configDir = options.configDir ? options.configDir : './';
+        var configFile = options.filename ? options.filename : '.env';
 
-                if (path.node.source.value === options.replacedModuleName) {
-                  var config = dotEnv.config({ path: sysPath.join(configDir, configFile), silent: true }) || {};
-                  var platformPath = (process.env.BABEL_ENV === 'development' || process.env.BABEL_ENV === undefined)
-                                          ? configFile + '.development'
-                                          : configFile + '.production';
-                  var config = Object.assign(config, dotEnv.config({ path: sysPath.join(configDir, platformPath), silent: true }));
+        if (path.node.source.value === options.replacedModuleName) {
+          var config = dotEnv.config({ path: sysPath.join(configDir, configFile), silent: true }) || {};
+          config = dotEnv.config({ path: sysPath.join(configDir, configFile+".local"), silent: true }) || {};
+          var platformPath = (process.env.BABEL_ENV === 'development' || process.env.BABEL_ENV === undefined)
+            ? configFile + '.development'
+            : configFile + '.production';
+          config = Object.assign(config, dotEnv.config({path: sysPath.join(configDir, platformPath), silent: true}));
+          config = Object.assign(config, dotEnv.config({
+            path: sysPath.join(configDir, platformPath + ".local"),
+            silent: true
+          }));
 
-                  path.node.specifiers.forEach(function(specifier, idx){
-                    if (specifier.type === "ImportDefaultSpecifier") {
-                      throw path.get('specifiers')[idx].buildCodeFrameError('Import dotenv as default is not supported.')
-                    }
-                    var importedId = specifier.imported.name
-                    var localId = specifier.local.name;
-                    if(!(config.hasOwnProperty(importedId))) {
-                      throw path.get('specifiers')[idx].buildCodeFrameError('Try to import dotenv variable "' + importedId + '" which is not defined in any ' + configFile + ' files.')
-                    }
-
-                    var binding = path.scope.getBinding(localId);
-                    binding.referencePaths.forEach(function(refPath){
-                      refPath.replaceWith(t.valueToNode(config[importedId]))
-                    });
-                  })
-
-                  path.remove();
-                }
+          path.node.specifiers.forEach(function(specifier, idx){
+            var localId = specifier.local.name;
+            var binding = path.scope.getBinding(localId);
+            if (specifier.type === "ImportDefaultSpecifier") {
+              binding.referencePaths.forEach(function(refPath){
+                refPath.replaceWith(t.valueToNode(config))
+              });
+            }else{
+              var importedId = specifier.imported.name
+              binding.referencePaths.forEach(function(refPath){
+                refPath.replaceWith(t.valueToNode(config[importedId]))
+              });
             }
+          })
+
+          path.remove();
         }
+      }
     }
-}
+  }
+};
